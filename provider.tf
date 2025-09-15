e5894cce74e7509ae08c09dae9dcc97f2110f3e9
João Alberto <jancunha@gmail.com>
2025-09-15 01:12:21 +0000

    Removendo a variável

--- provider.tf
+++ provider.tf
@@ -5,8 +5,12 @@ terraform {
       version = "~> 2.0"
     }
   }
+
+  # backend "pg" {
+  #   conn_str = "postgres://postgres:870441@192.168.0.6:5432/terraform"
+  # }
 }
 
 provider "digitalocean" {
-  token = var.do_token
+  token = {}
 }
